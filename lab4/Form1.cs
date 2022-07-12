9212368bf494cb457455b76c167139dde4068925
Kidus Mekuria <kidusmesfin27@gmail.com>
2022-07-12 20:17:27 +0300

    Edit 1
    If statement work

--- lab4/Form1.cs
+++ lab4/Form1.cs
@@ -28,51 +28,36 @@ namespace lab4
                 errorProvider1.SetError(txt_objectName, "Name not given");
                 valid = false;
             }
-            else
-            {
-                errorProvider1.Clear();
-            }
-            if (txt_number.Text.Length<3)
+            
+            else if (txt_number.Text.Length<3)
             {
                 
                 errorProvider1.SetError(txt_number, "number less than 3 characters");
                 valid = false;
             }
-            else
-            {
-                errorProvider1.Clear();
-            }
-            if (txt_inventoryNumber.Text.Length < 3)
+
+            else if (txt_inventoryNumber.Text.Length < 3)
             {
                 
                 errorProvider1.SetError(txt_inventoryNumber, "number less than 3 characters");
                 valid = false;
             }
-            else
-            {
-                errorProvider1.Clear();
-            }
-            if (txt_price.Text.Length < 3)
+
+            else if (txt_price.Text.Length < 3)
             {
                 
                 errorProvider1.SetError(txt_price, "number less than 3 characters");
                 valid = false;
             }
-            else
-            {
-                errorProvider1.Clear();
-            }
-            if (txt_count.Text.Length < 3)
+
+            else if (txt_count.Text.Length < 3)
             {
                 
                 errorProvider1.SetError(txt_count, "number less than 3 characters");
                 valid = false;
             }
-            else
-            {
-                errorProvider1.Clear();
-            }
-            if (string.IsNullOrEmpty(txt_date.Text))
+
+            else if (string.IsNullOrEmpty(txt_date.Text))
             {
                 
                 errorProvider1.SetError(txt_date, "Date not given");
@@ -82,19 +67,19 @@ namespace lab4
             {
                 errorProvider1.Clear();
             }
-            Item i = new Item
-            {
-                itemName = txt_objectName.Text,
-                number = int.Parse(txt_number.Text),
-                inventoryNumber = int.Parse(txt_inventoryNumber.Text),
-                price = Convert.ToDouble(txt_price.Text),
-                count = int.Parse(txt_count.Text),
-                date = txt_date.Text
-            };
+            
             if (valid)
             {
-               
-                
+                Item i = new Item
+                {
+                    itemName = txt_objectName.Text,
+                    number = int.Parse(txt_number.Text),
+                    inventoryNumber = int.Parse(txt_inventoryNumber.Text),
+                    price = Convert.ToDouble(txt_price.Text),
+                    count = int.Parse(txt_count.Text),
+                    date = txt_date.Text
+                };
+
                 i.save();
                 displayItem.DataSource = null;
                 displayItem.DataSource = Item.GetAll();
